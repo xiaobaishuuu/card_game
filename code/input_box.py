7227c17783968382650817db4311b8c9ac0f6934
xiaobaishuuu <xbs.mc01@gmail.com>
2024-09-20 17:45:09 +0800

    change

--- code/input_box.py
+++ code/input_box.py
@@ -18,7 +18,21 @@ class Input:
             pygame.draw.rect(screen,self.border_color,self.rect,8,25)
             if self.rect.collidepoint(event.pos) and pygame.MOUSEBUTTONDOWN:
                 while True:
-                    pass
-        except AttributeError:
-            pass
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                        try:
+                            if not box.collidepoint(event.pos) and event.type == pygame.MOUSEBUTTONDOWN:
+                                return
+                        except AttributeError:
+                            pass
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_BACKSPACE:
+                                user_text = user_text[:-1]
+                            else:
+                                user_text += event.unicode
+                                if not self.rect.collidepoint(event.pos) and pygame.MOUSEBUTTONDOWN:
+                                    return
+                    except AttributeError:
+                        pass
 loginPageInputs = [Input(SIGN_IN,USERNAME_BOX_RECT,(0,0,0),(83, 67, 67))]
